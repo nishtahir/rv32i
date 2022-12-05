a8a81ef044a2f1c30b54a00d41ec0c0d07e4919e
Nish Tahir <nishtahir@outlook.com>
2022-12-04 19:37:58 -0500

    Update schematics script to exclue NextRam

--- scripts/schematics.sh
+++ scripts/schematics.sh
@@ -1,7 +1,5 @@
-#! /bin/sh
-
-set -xe
-
+#! /bin/bash
+set -e
 BUILD_DIR=../build
 SCHEMATICS_DIR=../schematics
 SRC_DIR=../rtl
@@ -14,14 +12,18 @@ if [[ ! -e $SCHEMATICS_DIR ]]; then
     mkdir $SCHEMATICS_DIR
 fi
 
-for SRC_FILE in $SRC_DIR/*.sv ; do
-    MODULE=$(basename -s .sv "$SRC_FILE")
-    PREP_FILE=$BUILD_DIR/$MODULE.prep.json
-    SVG=$SCHEMATICS_DIR/$MODULE.prep.svg
-    yosys -q -p "read_verilog -sv -DSIMULATION $SRC_DIR/*.sv; prep -top $MODULE; write_json $PREP_FILE" $SRC_DIR/*.sv
-    netlistsvg "$PREP_FILE" -o "$SVG"
-    # PDF=$SCHEMATICS_DIR/$MODULE.prep.pdf
-    # inkscape --export-filename=$PDF $SVG
+for SRC_FILE in "$SRC_DIR"/*.sv ; do
+        MODULE=$(basename -s .sv "$SRC_FILE")
+    if [ "$MODULE" != "NextRam" ]; then
+        echo "[SCHEMATICS] Generating schematics for $MODULE..."
+        PREP_FILE=$BUILD_DIR/$MODULE.prep.json
+        SVG=$SCHEMATICS_DIR/$MODULE.prep.svg
+        yosys -q -p "read_verilog -sv -DSIMULATION $SRC_DIR/*.sv; prep -top $MODULE; write_json $PREP_FILE" $SRC_DIR/*.sv
+        netlistsvg "$PREP_FILE" -o "$SVG"
+        # PDF=$SCHEMATICS_DIR/$MODULE.prep.pdf
+        # inkscape --export-filename=$PDF $SVG
+    else
+        echo "[SCHEMATICS] Skipping schematics for NextRam..."
+    fi
 done
-
-set +xe
+set +e
